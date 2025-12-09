a20a7662c6195999221638043395b5e77ef346fb
Renaud <renaud@epitech.eu>
2025-12-09 14:05:16 +0100

    3

--- job_dsl.groovy
+++ job_dsl.groovy
@@ -28,7 +28,7 @@ job('Tools/SEED') {
 
     steps {
         dsl {
-            external('job_dsl.groovy')
+            external('/var/jenkins_home/job_dsl.groovy')
             removeAction('IGNORE')
         }
     }
