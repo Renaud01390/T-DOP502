64280a4acc44626fed7ccd30271c1effb5c995cb
Renaud <renaud@epitech.eu>
2025-12-09 11:32:56 +0100

    T-DOP502

---
+++ job_dsl.groovy
@@ -0,0 +1,72 @@
+folder('Tools') {
+    description('Folder for miscellaneous tools.')
+}
+
+job('Tools/clone-repository') {
+    description('Clone a Git repository specified by parameter.')
+
+    parameters {
+        stringParam('GIT_REPOSITORY_URL', '', 'Git URL of the repository to clone')
+    }
+
+    wrappers {
+        preBuildCleanup()
+    }
+
+    steps {
+        shell('git clone "$GIT_REPOSITORY_URL" .')
+    }
+}
+
+job('Tools/SEED') {
+    description('Seed job that creates a project job from given parameters.')
+
+    parameters {
+        stringParam('GITHUB_NAME', '', 'GitHub repository owner/repo_name')
+        stringParam('DISPLAY_NAME', '', 'Display name for the generated job')
+    }
+
+    steps {
+        dsl {
+            external('job_dsl.groovy')
+            removeAction('IGNORE')
+            additionalParameters([
+                GITHUB_NAME : '${GITHUB_NAME}',
+                DISPLAY_NAME : '${DISPLAY_NAME}'
+            ])
+        }
+    }
+}
+
+def githubName = binding.variables.get('GITHUB_NAME')
+def displayName = binding.variables.get('DISPLAY_NAME')
+
+if (githubName && displayName) {
+
+    job(displayName) {
+        description("Job generated for ${githubName}")
+
+        properties {
+            githubProjectUrl("https://github.com/${githubName}")
+        }
+
+        scm {
+            git("https://github.com/${githubName}.git")
+        }
+
+        triggers {
+            scm('* * * * *')
+        }
+
+        wrappers {
+            preBuildCleanup()
+        }
+
+        steps {
+            shell('make fclean')
+            shell('make')
+            shell('make tests_run')
+            shell('make clean')
+        }
+    }
+}
